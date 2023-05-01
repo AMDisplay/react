35846ec97e31eb82d2cdd93aeee9bbe8d69f663e
Display <melnikov933@mail.ru>
2023-05-01 18:48:08 +0300

    install tailwind

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './index.css';
 import Employee from './components/Employee';
 import {useState} from 'react'
 
@@ -6,7 +6,7 @@ function App() {
   const [role, setRole] = useState('dev');
   const showEmplyees = true;
   return (
-    <div className="App">
+    <div className="App bg-red-300">
       {showEmplyees ? (
       <>
       <input type='text' onChange={(e) => {
